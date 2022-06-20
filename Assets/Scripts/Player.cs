315985cc609914c41a6d48454a525c024d72163e
김현승 <corn120033@gmail.com>
2022-06-20 20:53:05 +0900

    Update Player.cs
    좌우 이동을 멈출 시 미끄러지는 이동을 멈춘다

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,12 +12,23 @@ public class Player : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        // Stop Speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(
+                rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+    }
+
     private void FixedUpdate()
     {
+        // Move Speed
         float h = Input.GetAxisRaw("Horizontal");
-
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
+        // Max Speed
         if (rigid.velocity.x > maxSpeed)
         {
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
