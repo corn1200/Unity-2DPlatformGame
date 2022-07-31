c119de9362258b0753328b0e3058c73f812b6d72
김현승 <corn120033@gmail.com>
2022-08-01 03:49:38 +0900

    Update Enemy.cs
    적 몹의 랜덤 이동 구현

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    public int nextMove;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Invoke("Think", 5);
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(-1, rigid.velocity.y);
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.2f, rigid.position.y);
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
+    }
+
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        if (nextMove != 0)
+        {
+            spriteRenderer.flipX = nextMove == 1;
+        }
+
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn()
+    {
+        nextMove *= -1;
+        spriteRenderer.flipX = nextMove == 1;
+        CancelInvoke();
+        Invoke("Think", 2);
     }
 }
